eb4a50368b510b17fb3fe90172c2cce585d79f1b
Suhich <miansu01@gmail.com>
2023-02-13 14:38:58 +0000

    fix: swagger post body

--- src/dogapi/dto/answerQuiz.dto.ts
+++ src/dogapi/dto/answerQuiz.dto.ts
@@ -1,11 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
 
 export class AnswerQuizDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'Id of the quiz',
+    example: '63ea4a235717c11d89691016',
+  })
   id: string;
 
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'Index of the answer: 0, 1 or 2',
+    example: '1',
+  })
   answer: number;
 }
