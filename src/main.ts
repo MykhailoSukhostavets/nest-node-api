106c4aeb993ff79fa2408066dc21fb4a86fab423
Suhich <miansu01@gmail.com>
2023-02-13 09:45:19 +0000

    feat: swagger, middleware logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Random Dogs')
+    .setDescription('Description for Dog API')
+    .setVersion('0.1')
+    .addTag('dog')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
